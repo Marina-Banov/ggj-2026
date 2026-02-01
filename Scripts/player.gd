a388241047b557b24e85049334e3a74ccc35ca73
tonto98 <rasanko@gmail.com>
2026-02-01 23:13:22 +0100

    limit damage taking, add dash with iframes, update color for hp

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,20 +1,50 @@
 extends CharacterBody2D
 
-const SPEED = 200
-const MAX_HP = 3
-
-var hp = MAX_HP
 @export var player_id = 0
 
+const SPEED = 200.0
+const DASH_SPEED = 600.0
+const DASH_DURATION = 0.15
+const DASH_COOLDOWN = 0.5
+const MAX_HP = 3
+const HP_COLORS = [
+	Color(0.09, 0.13, 0.25),
+	Color(0.68, 0.26, 0.28), 
+	Color(0.75, 0.69, 0.38), 
+	Color(0.29, 0.63, 0.38), 
+]
 
-func _physics_process(_delta: float) -> void:
-	var direction = Input.get_vector(
-		"move_left_{0}".format([player_id]),
-		"move_right_{0}".format([player_id]),
-		"move_up_{0}".format([player_id]),
-		"move_down_{0}".format([player_id])
-	)
-	velocity = direction * SPEED
+var hp = MAX_HP
+var dash_time_remaining = 0.0
+var dash_cooldown_remaining = 0.0
+var dash_direction = Vector2.ZERO
+var invulnerable = false
+
+
+func _ready() -> void:
+	_set_color_for_hp()
+
+
+func _physics_process(delta: float) -> void:
+	if dash_cooldown_remaining > 0:
+		dash_cooldown_remaining -= delta
+	else:
+		invulnerable = false
+	
+	if dash_time_remaining > 0:
+		dash_time_remaining -= delta
+		velocity = dash_direction * DASH_SPEED
+	else:
+		var direction = Input.get_vector(
+			"move_left_{0}".format([player_id]),
+			"move_right_{0}".format([player_id]),
+			"move_up_{0}".format([player_id]),
+			"move_down_{0}".format([player_id])
+		)
+		velocity = direction * SPEED
+		
+		if Input.is_action_just_pressed("dash_{0}".format([player_id])) and dash_cooldown_remaining <= 0 and direction != Vector2.ZERO:
+			_start_dash(direction)
 	move_and_slide()
 
 
@@ -23,6 +53,18 @@ func _on_hurt_box_area_entered(area: Area2D) -> void:
 		_take_damage()
 
 
-func _take_damage():
-	hp -= 1
-	print("player hp: ", hp)
+func _take_damage() -> void:
+	if hp > 0 and not invulnerable:
+		hp -= 1
+	_set_color_for_hp()
+
+
+func _start_dash(dir: Vector2) -> void:
+	invulnerable = true
+	dash_direction = dir
+	dash_time_remaining = DASH_DURATION
+	dash_cooldown_remaining = DASH_COOLDOWN
+
+
+func _set_color_for_hp() -> void:
+	%Polygon2D.color = HP_COLORS[hp]
