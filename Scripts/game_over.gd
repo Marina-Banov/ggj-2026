1dd1cee7930f023d64e69b08671fe3b5fb95affb
Marina-Banov <marina.banov@uniri.hr>
2026-02-06 20:57:25 +0100

    add main menu

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -5,6 +5,11 @@ func set_winner_text(player_id: int) -> void:
 	%Winner.text = "Player {0} won!".format([player_id])
 
 
-func _on_button_pressed() -> void:
+func _on_play_pressed() -> void:
 	get_tree().paused = false
 	get_tree().reload_current_scene()
+
+
+func _on_return_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
