0cc669838c07db9b3d183d695d59fd6df4d5f837
Marina-Banov <marina.banov@uniri.hr>
2026-02-22 12:31:30 +0100

    refactor progressive timer

--- Components/progressive_timer.gd
+++ Components/progressive_timer.gd
@@ -1,10 +1,9 @@
-extends Node2D
+extends Timer
 class_name ProgressiveTimer
 
-@export var start_interval = 1.0
-@export var min_interval = 0.1
-@export var duration = 60.0
-@onready var timer = $Timer
+@export_range(0.1, 10.0, 0.1) var start_interval = 1.0
+@export_range(0.1, 10.0, 0.1) var min_interval = 0.1
+@export_range(0.1, 300.0, 0.1) var duration = 60.0
 
 var elapsed_time = 0.0
 
@@ -12,15 +11,14 @@ signal progressed_timeout
 
 
 func _ready():
-	timer.timeout.connect(_on_timeout)
-	timer.start()
-
-
-func _process(delta):
-	elapsed_time += delta
-	var t = clamp(elapsed_time / duration, 0.0, 1.0)
-	timer.wait_time = lerp(start_interval, min_interval, t)
+	one_shot = false
+	wait_time = start_interval
+	timeout.connect(_on_timeout)
+	start()
 
 
 func _on_timeout():
+	elapsed_time += wait_time
+	var t = clamp(elapsed_time / duration, 0.0, 1.0)
+	wait_time = lerp(start_interval, min_interval, t)
 	progressed_timeout.emit()
